54f733c16de0847b06528f90517bc29a8a5c8e31
SRafi007 <sadmansakib.rafiaiub@gmail.com>
2023-05-14 21:19:38 +0600

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,19 +12,23 @@ import { EmployeeModule2 } from './Modules/employee2module.module';
 import {EmployeeModule } from './Modules/employee.module';
 import { BlogModule } from './Blog/blogmodule.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 //import { DoctorEntity } from './doctor/doctorentity.entity';
 
 
 @Module({
   imports: [CitizenModule,AdminModule,AllUsersSignUpForm,EmployeeModule2,EmployeeModule,BlogModule,DoctorModule,
           TypeOrmModule.forRoot({type: 'postgres',
-          host: 'containers-us-west-2.railway.app',
-          port: 6942,
+          host: 'containers-us-west-95.railway.app',
+          port: 7231,
           username: 'postgres',
-          password: 'qxCmXlXi6gN4t8OEoVZM',
+          password: 'q17wx1B97dhvne5vAJZW',
           database: 'railway',
           autoLoadEntities: true,
-          synchronize: true,})
+          synchronize: true,}), ServeStaticModule.forRoot({
+            rootPath: join (__dirname, '..', '../public'), // added ../ to get one folder back
+            serveRoot: '/public/' //last slash was important
+          }),
         ],
   controllers: [AppController],
   providers: [AppService],
